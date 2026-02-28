82b250773b2d0ec8402bcdb7be66bcc2afd51ac1
Allenjonesing <Allenjonesing@gmail.com>
2026-02-27 19:08:44 -0600

    Default

---
+++ vehicles/default_dummy/materials.cs
@@ -0,0 +1,15 @@
+
+singleton Material(dummy_mat)
+{
+   mapTo = "dummy";
+   diffuseMap[0] = "vehicles/dummy/Dummy.png";
+   specular[0] = "0.996078 0.996078 0.996078 1";
+   specularPower[0] = "128";
+   translucentBlendOp = "None";
+   detailScale[0] = "4 4";
+   materialTag0 = "Miscellaneous";
+   detailMapScale0 = "5";
+   beamngDiffuseColorSlot = "2";
+   pixelSpecular[0] = "1";
+};
+
